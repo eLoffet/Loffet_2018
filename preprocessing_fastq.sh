bba362b020be8d6110e428197a42e80a5eb4fb9e
eLoffet <34254314+eLoffet@users.noreply.github.com>
2022-02-14 01:09:52 +0100

    Update preprocessing_fastq.sh

--- preprocessing_fastq.sh
+++ preprocessing_fastq.sh
@@ -1,18 +1,21 @@
 ### quality control with fastqc
-for i in *.fastq.gz; do 
-fastqc $i
+for i in *.fastq.gz
+do 
+  fastqc $i
 done
 
 ###cut adapter sequences from reads with cutadapt
 #single read data:
-for i in *.fastq.gz; do 
-trim_galore --nextera $i
+for i in *.fastq.gz
+do 
+  trim_galore --nextera $i
 done
 
 #paired-end data:
-for read1 in read1*.fastq.gz; do 
-read2=$(echo $read1| sed 's/read1/read2/g')
-trim_galore --paired --nextera $read1 $read2
+for read1 in read1*.fastq.gz
+do 
+  read2=$(echo $read1| sed 's/read1/read2/g')
+  trim_galore --paired --nextera $read1 $read2
 done
 
 
@@ -21,25 +24,29 @@ wget <urlgenomehisat2>
 tar zxvf genome.tar.gz
 
 #single read data:
-for i in *.fq.gz; do
-hisat2 -q -x genome_tran -U $i -S $i.sam
+for i in *.fq.gz
+do
+  hisat2 -q -x genome_tran -U $i -S $i.sam
 done
 
 #paired-end data:
-for read1 in read1*.fq.gz; do 
-read2=$(echo $read1| sed 's/read1/read2/g' | sed 's/val_1/val_2/g')
-hisat2 -q -x genome_tran -1 $read1 -2 $read2 -S $read1.sam
+for read1 in read1*.fq.gz
+do 
+  read2=$(echo $read1| sed 's/read1/read2/g' | sed 's/val_1/val_2/g')
+  hisat2 -q -x genome_tran -1 $read1 -2 $read2 -S $read1.sam
 done
 
 
 ###convert sam to bam
-for i in *.sam;do
-samtools view -bS -o $i.bam $i
+for i in *.sam
+do
+  samtools view -bS -o $i.bam $i
 done
 
 ###sort bam files
-for i in *.bam;do
-samtools sort -o sorted.$i $i
+for i in *.bam
+do
+  samtools sort -o sorted.$i $i
 done
 
 ###get read counts matrix with featurecounts
